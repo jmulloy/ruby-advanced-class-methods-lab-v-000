9c184edb07444281befeb73e954860beeac4dc49
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-04 00:18:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.create_by_name(song_name)
     new_song_2 = self.new
     new_song_2.name = song_name
-    new_song_2 = self.new_by_name(song_name)
+    # new_song_2 = self.new_by_name(song_name)
     new_song_2.save
     new_song_2
   end
